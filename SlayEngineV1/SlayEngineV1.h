586c3dc7ea219699e2fbf89aa30a7e11b6801168
zanadoman <domanseven@gmail.com>
2023-11-26 23:25:58 +0100

    .

--- SlayEngineV1/SlayEngineV1.h
+++ SlayEngineV1/SlayEngineV1.h
@@ -1,6 +1,7 @@
 #include <SDL2/SDL.h> //UNIX PORT
 #include <SDL2/SDL_image.h> //UNIX PORT
 //#include "inc/SDL.h" //WINDOWS PORT
+//#include "inc/SDL_image.h" //WINDOWS PORT
 #include <pthread.h>
 #include "NeoTypes/NeoTypes.h"
 
