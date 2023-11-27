5305a4fdc5304344d81bd7fd322561a1526b4549
zanadoman <domanseven@gmail.com>
2023-11-27 06:27:15 +0100

    .

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-gcc -o ./build/bin *.c SlayEngineV1/*.c SlayEngineV1/NeoTypes/*.c -lSDL2 -lSDL2_image -lm && ./build/bin
+time gcc -o ./build/bin *.c SlayEngineV1/*.c SlayEngineV1/NeoTypes/*.c -lSDL2 -lSDL2_image -lm && ./build/bin
 cp -r assets ./build
