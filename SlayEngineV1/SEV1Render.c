e1bac8723789ea8d35b30d9dfbd695c82b1a542d
zanadoman <domanseven@gmail.com>
2023-11-28 12:21:48 +0100

    .

--- SlayEngineV1/SEV1Render.c
+++ SlayEngineV1/SEV1Render.c
@@ -16,6 +16,7 @@ SDL_Texture* slayLoadTexture(slayDisplay* Display, char* Path)
         exit(1);
     }
     result = SDL_CreateTextureFromSurface(Display->Renderer, surface);
+    
     SDL_FreeSurface(surface);
 
     return result;
