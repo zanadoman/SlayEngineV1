11b3dab6e47941a1a0278f628ca09bb7b46e300c
zanadoman <domanseven@gmail.com>
2023-11-28 15:16:23 +0100

    .

--- build.sh
+++ build.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 TIMEFORMAT=%R
-buildtime=$(time (gcc -o ./build/bin *.c SlayEngineV1/*.c SlayEngineV1/NeoTypes/*.c -lSDL2 -lSDL2_image -lSDL2_ttf -lSDL2_mixer -lm) 2>&1 1>/dev/null)
+buildtime=$(time (gcc -o ./build/bin *.c SlayEngineV1/*.c SlayEngineV1/NeoTypes/*.c -lSDL2 -lSDL2_image -lSDL2_ttf -lSDL2_mixer -lm) 2>&1)
 
 if [ $? == 0 ]
 then
-    echo -e "Build successful ${buildtime}"
+    echo -e "Build successful! ${buildtime}s"
     ./build/bin
 
     if [ $? == 0 ]
     then
-        echo "Run successful, copying assets"
+        echo "Run successful!"
         cp -r assets ./build
         exit 0
     fi
     
-    echo "Run failed"
+    echo "Run failed!"
     exit 1
 fi
 
-echo -e "Build failed"
+echo -e "Build failed!"
 exit 1
