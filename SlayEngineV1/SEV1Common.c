93f25846e5565e1027c4d8645ca82ca99c92753c
zanadoman <domanseven@gmail.com>
2023-11-28 12:12:17 +0100

    .

--- SlayEngineV1/SEV1Common.c
+++ SlayEngineV1/SEV1Common.c
@@ -7,7 +7,9 @@ slayDisplay* slayNew(char* Title, int Width, int Height)
     SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO);
     TTF_Init();
     Mix_OpenAudio(MIX_DEFAULT_FREQUENCY, MIX_DEFAULT_FORMAT, MIX_DEFAULT_CHANNELS, 4096);
+
     result = malloc(sizeof(slayDisplay));
+    
     result->Width = Width;
     result->Height = Height;
     result->Window = SDL_CreateWindow(Title, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, result->Width, result->Height, 0);
