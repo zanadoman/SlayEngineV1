4ec0557ac6fa70073fe4e4f1e3e60648013c57a1
zanadoman <domanseven@gmail.com>
2023-11-28 17:08:35 +0100

    .

--- update.c
+++ update.c
@@ -6,28 +6,12 @@ void* updateProjectileThread(void* Game);
 
 uint16 updateQueue(game* Game)
 {
-    //0.
-    updateZoomIn(Game);
     //1.
     pthread_create((pthread_t*)Game->Threads->Values[0], NULL, updatePlayerThread, (void*)Game);
     pthread_create((pthread_t*)Game->Threads->Values[1], NULL, updateProjectileThread, (void*)Game);
     pthread_join(*(pthread_t*)Game->Threads->Values[0], NULL);
     pthread_join(*(pthread_t*)Game->Threads->Values[1], NULL);
     //2.
-
-    return 0;
-}
-
-uint16 updateZoomIn(game* Game)
-{
-    if (Game->Camera->Zoom < 1)
-    {
-        Game->Camera->Zoom += 0.0002 * Game->DeltaTime;
-    }
-    else if (Game->Camera->Zoom > 1)
-    {
-        Game->Camera->Zoom = 1;
-    }
 
     return 0;
 }
