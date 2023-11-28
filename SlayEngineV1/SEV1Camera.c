e6db05702e16084da6ec03957f0521380782d897
zanadoman <domanseven@gmail.com>
2023-11-28 12:08:37 +0100

    .

--- SlayEngineV1/SEV1Camera.c
+++ SlayEngineV1/SEV1Camera.c
@@ -5,7 +5,7 @@ slayCamera* slayNewCamera(double* OriginX, double* OriginY, double RelativeX, do
     slayCamera* result;
 
     result = malloc(sizeof(slayCamera));
-    
+
     result->OriginX = OriginX;
     result->OriginY = OriginY;
     result->RelativeX = RelativeX;
@@ -28,8 +28,6 @@ uint16 slayApplyCamera(SDL_Rect* Object, slayCamera* Camera, double X, double Y)
 {
     Object->x = (sint32)round(X - Camera->AbsoluteX);
     Object->y = (sint32)round(Y - Camera->AbsoluteY);
-    Object->w = Object->w;
-    Object->h = Object->h;
 
     return 0;
 }
