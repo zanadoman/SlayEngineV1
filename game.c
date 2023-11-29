d55791d2dfb95d2bcf40aa6cfd5943c5e4ef2c21
zanadoman <domanseven@gmail.com>
2023-11-30 00:17:56 +0100

    .

--- game.c
+++ game.c
@@ -9,11 +9,9 @@ int main(int argc, char *argv[])
     Game = malloc(sizeof(game));
     loadGame(Game);
 
-    while(slayEvent(Game->Display, Game->Mouse) != 0)
+    while(slayEvent(Game->Display, Game->Mouse))
     {
         Game->DeltaTime = slayDeltaTime(&Game->DisplayPrevTick);
-
-        printf("x: %d, y: %d, lmb: %d, mmb: %d, rmb: %d, wheel: %d\n", Game->Mouse->X, Game->Mouse->Y, Game->Mouse->LMB, Game->Mouse->MMB, Game->Mouse->RMB, Game->Mouse->Wheel);
 
         updateQueue(Game);
         renderQueue(Game);
