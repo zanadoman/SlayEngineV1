fd695a8ec297b7fc796533f98f3ca192b0e88570
zanadoman <domanseven@gmail.com>
2023-12-03 15:11:14 +0100

    .

--- main.c
+++ main.c
@@ -1,5 +1,4 @@
 /*
-Player coordinátáinak elmentése, behúzása
 Wiki írása
 */
 
@@ -16,6 +15,14 @@ uint16 main(uint64 argc, char* *argv)
 
     Engine->CurrentScene = 0;
 
+    array save = arrNew(0);
+    fileRead("save.txt", save);
+    ((scene0*)Engine->Scenes->Values[0])->Player->X = STRtoDOUBLE(((string)save->Values[0])->String, NULL);
+    ((scene0*)Engine->Scenes->Values[0])->Player->Y = STRtoDOUBLE(((string)save->Values[1])->String, NULL);
+    strPurge(save->Values[0]);
+    strPurge(save->Values[1]);
+    arrPurge(save);
+
     while(slayEvent(Engine))
     {
         slayUpdateDeltaTime(Engine);
@@ -25,6 +32,14 @@ uint16 main(uint64 argc, char* *argv)
 
         slayCapFPS(Engine);
     }
+
+    save = arrNew(2);
+    save->Values[0] = strNew();
+    save->Values[1] = strNew();
+    DOUBLEtoSTR(((scene0*)Engine->Scenes->Values[0])->Player->X, save->Values[0]);
+    DOUBLEtoSTR(((scene0*)Engine->Scenes->Values[0])->Player->Y, save->Values[1]);
+    printf("%s\n", ((string)save->Values[0])->String);
+    fileWrite(save, "save.txt");
 
     return 0;
 }
