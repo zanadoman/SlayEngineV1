4a6b13cabcf8771ef35c5e3cb7b96aa1d5912db2
zanadoman <domanseven@gmail.com>
2023-12-04 20:02:47 -0800

    .

--- build.cmd
+++ build.cmd
@@ -1,6 +1,6 @@
 @echo off
 
-gcc -o ./Windows/bin.exe *.c ./SlayEngineV1/*.c ./SlayEngineV1/NeoTypes/*.c -mwindows -L./SlayEngineV1/lib -lSDL2 -L./SlayEngineV1/lib -lSDL2_image -L./SlayEngineV1/lib -lSDL2_ttf -L./SlayEngineV1/lib -lSDL2_mixer -lpthread -lm
+gcc -o Windows/bin.exe *.c SlayEngineV1/*.c SlayEngineV1/NeoTypes/*.c -mwindows -LSlayEngineV1/lib -lSDL2 -LSlayEngineV1/lib -lSDL2_image -LSlayEngineV1/lib -lSDL2_ttf -LSlayEngineV1/lib -lSDL2_mixer -lpthread -lm
 if %errorlevel%==0 (
 	cd Windows
 	start ./bin
