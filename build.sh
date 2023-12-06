f2cdd91b598e55d1b84e6b0c415c45bf6fa07740
zanadoman <domanseven@gmail.com>
2023-12-06 19:58:45 +0100

    .

--- build.sh
+++ build.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
-function compile()
-{
+if [ ! -z $1 ] && [ $1 == "-a" ] || [ ! -z $1 ] && [ $1 == "--all" ]
+then
+    cd Compiled
+    rm *.o
+    gcc -c ../Demo/*.c ../Demo/Actors/*.c ../Demo/Scenes/*.c ../SlayEngineV1/*.c ../SlayEngineV1/NeoTypes/*.c
+    if [ $? != 0 ]
+    then
+        echo "Re-compilation failed!"
+        exit 1
+    fi
+    cd ..
+    echo "Re-compilation successful!"
+else
     git diff --name-only | grep .c 1> /dev/null
     if [ $? == 0 ]
     then
@@ -13,23 +24,20 @@ function compile()
             exit 1
         fi
         cd ..
+        echo "Pre-compilation successful!"
     else
-        echo "Compilation skipped!"
-        return 0;
+        echo "Pre-compilation skipped!"
     fi
+fi
 
-    gcc -o Linux/bin Compiled/*.o -Wl,-rpath=. -lSDL2 -lSDL2_image -lSDL2_ttf -lSDL2_mixer -lm
-    if [ $? != 0 ]
-    then
-        echo "Compilation failed!"
-        exit 1
-    fi
-
+gcc -o Linux/bin Compiled/*.o -Wl,-rpath=. -lSDL2 -lSDL2_image -lSDL2_ttf -lSDL2_mixer -lm
+if [ $? != 0 ]
+then
+    echo "Compilation failed!"
+    exit 1
+else
     echo "Compilation successful!"
-    return 0
-}
-
-compile
+fi
 
 cd Linux
 ./bin
