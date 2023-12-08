1d55fcbd53f16804f1b324dabb8cacd332bffa09
zanadoman <domanseven@gmail.com>
2023-12-08 20:23:08 +0100

    .

--- build.sh
+++ build.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
-projectdir="$(pwd)"
+RED="\e[31m"
+GREEN="\e[32m"
+ENDCOLOR="\e[0m"
 
 if [[ ! -z $1 && $1 == "-a" ]] || [[ ! -z $1 && $1 == "--all" ]]
 then
     gcc -c Demo/*.c Demo/Actors/*.c Demo/Scenes/*.c SlayEngineV1/*.c SlayEngineV1/NeoTypes/*.c
     if [ $? != 0 ]
     then
-        echo "Re-compilation failed!"
+        echo -e "${RED}Re-compilation failed!${ENDCOLOR}"
         rm *.o
         exit 1
     fi
     rm Compiled/*.o
     mv *.o Compiled
-    echo "Re-compilation successful!"
+    echo -e "${GREEN}Re-compilation successful!${ENDCOLOR}"
 else
     git diff --name-only | grep "\.c" 1> /dev/null
     if [ $? == 0 ]
@@ -21,32 +23,32 @@ else
         gcc -c $(git diff --name-only | grep "\.c")
         if [ $? != 0 ]
         then
-            echo "Pre-compilation failed!"
+            echo "${RED}Pre-compilation failed!${ENDCOLOR}"
             exit 1
         fi
         mv *.o Compiled
-        echo "Pre-compilation successful!"
+        echo -e "${GREEN}Pre-compilation successful!${ENDCOLOR}"
     else
-        echo "Pre-compilation skipped!"
+        echo -e "${GREEN}Pre-compilation skipped!${ENDCOLOR}"
     fi
 fi
 
 gcc -o Linux/bin Compiled/*.o -Wl,-rpath=. -lSDL2 -lSDL2_image -lSDL2_ttf -lSDL2_mixer -lm
 if [ $? != 0 ]
 then
-    echo "Compilation failed!"
+    echo -e "${RED}Compilation failed!${ENDCOLOR}"
     exit 1
 else
-    echo "Compilation successful!"
+    echo -e "${GREEN}Compilation successful!${ENDCOLOR}"
 fi
 
 cd Linux
 ./bin
 if [ $? == 0 ]
 then
-    echo "Run successful!"
+    echo -e "${GREEN}Run successful!${ENDCOLOR}"
     exit 0
 else
-    echo "Run failed!"
+    echo -e "${RED}Run failed!${ENDCOLOR}"
     exit 1
 fi
