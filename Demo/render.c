6385259d143307e27976df7b56fb0993e70d2fa1
zanadoman <domanseven@gmail.com>
2023-12-14 19:02:25 +0100

    .

--- Demo/render.c
+++ Demo/render.c
@@ -69,7 +69,7 @@ uint8 renderFrameTime(slayEngine* Engine)
     text = strNew();
     UINTtoSTR(Engine->DeltaTime, text);
     strConcat(text, 3, "Frametime: ", text->String, "ms");
-    slayRenderText(Engine, ((game*)Engine->Game)->FontCrazyPixel, " ", 10, -10, 0.75, 0, slayFlipNONE, 0, 0, 0, 255);
+    slayRenderText(Engine, ((game*)Engine->Game)->FontCrazyPixel, text->String, 10, -10, 0.75, 0, slayFlipNONE, 0, 0, 0, 255);
     strPurge(text);
 
     return 0;
