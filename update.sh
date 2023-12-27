35d3dbc4698da4a1a5107726b7ea0315da79cfa6
zanadoman <domanseven@gmail.com>
2023-12-27 22:58:54 +0100

    .

---
+++ update.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+RED="\e[91m"
+GREEN="\e[92m"
+BLUE="\e[94m"
+ENDCOLOR="\e[0m"
+
+echo "Updating NeoTypes"
+
+echo -e "${BLUE}NeoTypes.h${ENDCOLOR}"
+curl -s "https://github.com/zanadoman/NeoTypes/blob/main/Source/NeoTypes.h" -o SlayEngineV1/Includes/NeoTypes.h
+if [ $? == 0 ]
+then
+    echo -e "${GREEN}NeoTypes.h updated!${ENDCOLOR}"
+else
+    echo -e "${RED}NeoTypes.h update failed!${ENDCOLOR}"
+fi
+
+echo -e "${BLUE}libNeoTypes.a (Linux)${ENDCOLOR}"
+curl -s "https://github.com/zanadoman/NeoTypes/blob/main/Linux%20(x86_64)/libNeoTypes.a" -o SlayEngineV1/Libraries/Linux/libNeoTypes.a
+if [ $? == 0 ]
+then
+    echo -e "${GREEN}libNeoTypes.a (Linux) updated!${ENDCOLOR}"
+else
+    echo -e "${RED}libNeoTypes.a update (Linux) failed!${ENDCOLOR}"
+fi
+
+echo -e "${BLUE}libNeoTypes.a (Windows)${ENDCOLOR}"
+curl -s "https://github.com/zanadoman/NeoTypes/blob/main/Windows%20(x86_64)/libNeoTypes.a" -o SlayEngineV1/Libraries/Windows/libNeoTypes.a
+if [ $? == 0 ]
+then
+    echo -e "${GREEN}libNeoTypes.a (Windows) updated!${ENDCOLOR}"
+else
+    echo -e "${RED}libNeoTypes.a (Windows) update failed!${ENDCOLOR}"
+fi
