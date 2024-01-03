0f3e94852779e628860f91aca8a0fcb4e99858ca
zanadoman <domanseven@gmail.com>
2024-01-03 20:15:25 +0100

    .

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ ENDCOLOR="\e[0m"
 
 if [[ ! -z $1 && $1 == "-w" ]] || [[ ! -z $1 && $1 == "--windows" ]]
 then
-    x86_64-w64-mingw32-gcc -o Builds/Windows/bin.exe $(find . -name '*.c') -mwindows -LSlayEngineV1/Libraries/Windows -lSDL2 -lSDL2_image -lSDL2_ttf -lSDL2_mixer -lNeoTypes -lcJSON -lm
+    x86_64-w64-mingw32-gcc -o Builds/Windows/bin.exe $(find . -name '*.c') -mwindows -LSlayEngineV1/Libraries/Windows -lSDL2 -lSDL2_image -lSDL2_ttf -lSDL2_mixer -lNeoTypes -lm
     if [ $? == 0 ]
     then
         echo -e "${BLUE}Windows ${GREEN}build successful!${ENDCOLOR}"
@@ -59,7 +59,7 @@ else
     fi
 fi
 
-gcc -o Builds/Linux/bin.out Compiled/*.o -Wl,-rpath=. -LSlayEngineV1/Libraries/Linux -lfreetype -lSDL2 -lSDL2_image -lSDL2_ttf -lSDL2_mixer -lNeoTypes -lcJSON -lm
+gcc -o Builds/Linux/bin.out Compiled/*.o -Wl,-rpath=. -LSlayEngineV1/Libraries/Linux -lfreetype -lSDL2 -lSDL2_image -lSDL2_ttf -lSDL2_mixer -lNeoTypes -lm
 if [ $? == 0 ]
 then
     echo -e "${GREEN}Compilation successful!${ENDCOLOR}"
