d402a3e236f01e5c86f71dc020acb9304eeef9c3
zanadoman <domanseven@gmail.com>
2024-01-03 11:05:08 +0100

    .

--- SlayEngineV1/SEV1Audio.c
+++ SlayEngineV1/SEV1Audio.c
@@ -4,10 +4,16 @@ slaySound* slayLoadSound(char* Path)
 {
     Mix_Chunk* result;
 
+    if (Path == NULL)
+    {
+        printf("slayLoadSound(): Path must not be NULL\nParams: Path: %p\n", Path);
+        exit(1);
+    }
+
     result = Mix_LoadWAV(Path);
     if (result == NULL)
     {
-        printf("ERROR Unable to load SOUND: %s\n", Path);
+        printf("slayLoadSound(): Unable to load Sound\nParams: Path: %s\n", Path);
         exit(1);
     }
 
@@ -16,15 +22,26 @@ slaySound* slayLoadSound(char* Path)
 
 uint8 slayPlaySound(slaySound* Sound, sint16 Channel, uint8 Volume, uint8 Left, uint8 Right, sint16 Loops)
 {
+    if (Sound == NULL)
+    {
+        printf("slayPlaySound(): Sound must not be NULL\nParams: Sound: %p, Channel: %d, Volume: %d, Left: %d, Right: %d, Loops: %d\n", Sound, Channel, Volume, Left, Right, Loops);
+        exit(1);
+    }
+    if (Loops < -1)
+    {
+        printf("slayPlaySound(): Loops must not be less than -1\nParams: Sound: %p, Channel: %d, Volume: %d, Left: %d, Right: %d, Loops: %d\n", Sound, Channel, Volume, Left, Right, Loops);
+        exit(1);
+    }
+
     Mix_VolumeChunk(Sound, Volume);
     if (Mix_PlayChannel(Channel, Sound, Loops) == -1)
     {
-        printf("ERROR Unable to play SOUND (%d)", Channel);
+        printf("slayPlaySound(): Unable to play Sound\nParams: Sound: %p, Channel: %d, Volume: %d, Left: %d, Right: %d, Loops: %d\n", Sound, Channel, Volume, Left, Right, Loops);
         exit (1);
     }
     if (Mix_SetPanning(Channel, Left, Right) == 0)
     {
-        printf("ERROR Unable to set SOUND_PANNING (%d)", Channel);
+        printf("slayPlaySound(): Unable to set Sound Panning\nParams: Sound: %p, Channel: %d, Volume: %d, Left: %d, Right: %d, Loops: %d\n", Sound, Channel, Volume, Left, Right, Loops);
         exit (1);
     }
 
@@ -33,15 +50,26 @@ uint8 slayPlaySound(slaySound* Sound, sint16 Channel, uint8 Volume, uint8 Left, 
 
 uint8 slayPlaySoundTicks(slaySound* Sound, sint16 Channel, uint8 Volume, uint8 Left, uint8 Right, sint16 Loops, uint32 Ticks)
 {
+    if (Sound == NULL)
+    {
+        printf("slayPlaySoundTicks(): Sound must not be NULL\nParams: Sound: %p, Channel: %d, Volume: %d, Left: %d, Right: %d, Loops: %d, Ticks: %d\n", Sound, Channel, Volume, Left, Right, Loops, Ticks);
+        exit(1);
+    }
+    if (Loops < -1)
+    {
+        printf("slayPlaySoundTicks(): Loops must not be less than -1\nParams: Sound: %p, Channel: %d, Volume: %d, Left: %d, Right: %d, Loops: %d, Ticks: %d\n", Sound, Channel, Volume, Left, Right, Loops, Ticks);
+        exit(1);
+    }
+
     Mix_VolumeChunk(Sound, Volume);
     if (Mix_PlayChannelTimed(Channel, Sound, Loops, Ticks) == -1)
     {
-        printf("ERROR Unable to play SOUND (%d)", Channel);
+        printf("slayPlaySoundTicks(): Unable to play Sound\nParams: Sound: %p, Channel: %d, Volume: %d, Left: %d, Right: %d, Loops: %d, Ticks: %d\n", Sound, Channel, Volume, Left, Right, Loops, Ticks);
         exit (1);
     }
     if (Mix_SetPanning(Channel, Left, Right) == 0)
     {
-        printf("ERROR Unable to set SOUND_PANNING (%d)", Channel);
+        printf("slayPlaySoundTicks(): Unable to set Sound Panning\nParams: Sound: %p, Channel: %d, Volume: %d, Left: %d, Right: %d, Loops: %d, Ticks: %d\n", Sound, Channel, Volume, Left, Right, Loops, Ticks);
         exit (1);
     }
 
