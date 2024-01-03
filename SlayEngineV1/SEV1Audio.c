6bad2724708a30483587306c400f901b9016d0d9
zanadoman <domanseven@gmail.com>
2024-01-03 16:16:30 +0100

    .

--- SlayEngineV1/SEV1Audio.c
+++ SlayEngineV1/SEV1Audio.c
@@ -4,12 +4,6 @@ slaySound* slayLoadSound(char* Path)
 {
     Mix_Chunk* result;
 
-    if (Path == NULL)
-    {
-        printf("slayLoadSound(): Path must not be NULL\nParams: Path: %p\n", Path);
-        exit(1);
-    }
-
     result = Mix_LoadWAV(Path);
     if (result == NULL)
     {
@@ -22,11 +16,6 @@ slaySound* slayLoadSound(char* Path)
 
 uint8 slayPlaySound(slaySound* Sound, sint16 Channel, uint8 Volume, uint8 Left, uint8 Right, sint16 Loops)
 {
-    if (Sound == NULL)
-    {
-        printf("slayPlaySound(): Sound must not be NULL\nParams: Sound: %p, Channel: %d, Volume: %d, Left: %d, Right: %d, Loops: %d\n", Sound, Channel, Volume, Left, Right, Loops);
-        exit(1);
-    }
     if (Channel < -1)
     {
         printf("slayPlaySound(): Channel must not be less than -1\nParams: Sound: %p, Channel: %d, Volume: %d, Left: %d, Right: %d, Loops: %d\n", Sound, Channel, Volume, Left, Right, Loops);
@@ -55,11 +44,6 @@ uint8 slayPlaySound(slaySound* Sound, sint16 Channel, uint8 Volume, uint8 Left, 
 
 uint8 slayPlaySoundTicks(slaySound* Sound, sint16 Channel, uint8 Volume, uint8 Left, uint8 Right, sint16 Loops, uint32 Ticks)
 {
-    if (Sound == NULL)
-    {
-        printf("slayPlaySoundTicks(): Sound must not be NULL\nParams: Sound: %p, Channel: %d, Volume: %d, Left: %d, Right: %d, Loops: %d, Ticks: %d\n", Sound, Channel, Volume, Left, Right, Loops, Ticks);
-        exit(1);
-    }
     if (Channel < -1)
     {
         printf("slayPlaySoundTicks(): Channel must not be less than -1\nParams: Sound: %p, Channel: %d, Volume: %d, Left: %d, Right: %d, Loops: %d, Ticks: %d\n", Sound, Channel, Volume, Left, Right, Loops, Ticks);
